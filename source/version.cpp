940d8478720d41072ecb6e3a01f121bf7c37b6de
Marios Chatzikos <mchatzikos@gmail.com>
2020-12-11 21:56:06 -0500

    Enable use of git tags in printed Cloudy versions
    As part of the switch to git, a new workflow is to be established.
    The previous practice of releasing every few years would better be
    replaced by a scheme where we release every few months.  We seem to be
    gravitating toward a model where feature branches are merged onto
    master, which is merged onto a release branch for periodical releases.
    
    The Cloudy version reported in the .out file treats the release branch
    as any other, which would be opaque to users.  The version number stored
    as a tag to the appropriate git commit is now employed as the Cloudy
    version in user-facing runs.
    
    Reported-by: M. Dehghanian
    https://groups.google.com/g/cloudy-dev/c/N_tKJTriDDw

--- source/version.cpp
+++ source/version.cpp
@@ -6,7 +6,7 @@
 #include "version.h"
 #include "service.h"
 
-static const int CLD_MAJOR = 13;
+static const int CLD_MAJOR = 21;
 static const int CLD_MINOR = 0;
 static const int CLD_PATCH = 0;
 
@@ -16,38 +16,29 @@ static const char* revision = REVISION;
 static const char* revision = "rev_not_set";
 #endif
 
-static const char* cUrl = "$HeadURL$";
+static const string release_branch = "release";
 
 t_version::t_version()
 {
 	static const char chMonth[12][4] =
 		{ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
-	// first analyze the URL to determine where we live, the chVersion string is derived from that
-	// the code below is based on the following naming scheme:
-	//
-	// /branches/c08_branch -- release branch, all bug fixes are submitted here 
-	//
-	// /tags/develop/c08.00_rc1 -- release candidates go here 
-	//
-	// /tags/release/c08.00 -- first official release
-	// /tags/release/c08.01 -- first bug-fix rollup, etc... 
-	//
-	// /tags/patch_versions/c08.00_pl00 -- identical to /tags/release/c08.00
-	// /tags/patch_versions/c08.00_pl01 -- first patch update, etc... 
-	//
-	// /trunk -- this will be labeled as "experimental"
-	// /tags/stable -- ditto
-	// /branches/* -- ditto, note that "*" can be anything except c??_branch
+	// REVISION is a dash-delimited string; it may consist of the branch name,
+	// followed by a tag, or by a git SHA1 commit string, and a modification
+	// identifier
 
-	vector<string> Part;
-	string Url = cUrl;
-	Split( Url, "/", Part, SPM_RELAX );
 	if( strcmp( revision, "rev_not_set" ) != 0 )
 	{
 		vector<string> Part;
 		string rev = revision;
 		Split( rev, "-", Part, SPM_RELAX );
+
+		if( Part[ 0 ] == release_branch )
+		{
+			lgRelease = lgReleaseBranch = true;
+			Part.erase( Part.begin() );
+		}
+
 		string rev_pr = "";
 		for( size_t i = 0 ; i < Part.size(); i++ )
 		{
@@ -58,87 +49,9 @@ t_version::t_version()
 		}
 		chVersion = "(" + string( rev_pr ) + ")";
 	}
-	else if( false )
-	{
-		// the last two parts are "source" and "version.cpp $", we don't need them...
-		// the one before is the relevant identifier (e.g. "trunk", "newmole", "c08.01")
-		// for conciseness we will refer to it below as the "branch"
-		string Branch = Part[Part.size()-3];
-
-		bool lgReleaseTag = ( Url.find("/tags/release/") != string::npos );
-		bool lgPatchTag = ( Url.find("/tags/patch_versions/") != string::npos );
-		bool lgDevelopTag = ( Url.find("/tags/develop/") != string::npos );
-		// this expects a branch name like "c08_branch"
-		lgReleaseBranch = ( Url.find("/branches/") != string::npos &&
-				    Branch.size() == 10 && Branch[0] == 'c' &&
-				    Branch.find("_branch") != string::npos );
-
-		lgRelease = ( lgReleaseTag || lgPatchTag );
-
-		// determine if this is a beta version
-		string::size_type ptr;
-		if( lgDevelopTag && ( ptr = Branch.find( "_rc" ) ) != string::npos )
-			// this expects a branch name like "c08.00_rc1"
-			sscanf( Branch.substr( ptr+3 ).c_str(), "%ld", &nBetaVer );
-		else
-			nBetaVer = 0;
-
-		// beta versions are always generated from the release branch...
-		if( nBetaVer > 0 )
-			lgReleaseBranch = true;
-
-		int nMajorLevel=0, nMinorLevel=0, nPatchLevel=0;
-
-		if( lgReleaseBranch || lgRelease || nBetaVer > 0 )
-		{
-			// this expects a branch name starting with "c08"
-			sscanf( Branch.substr(1,2).c_str(), "%d", &nMajorLevel );
-			if( nMajorLevel != CLD_MAJOR )
-				fprintf( ioQQQ, "PROBLEM - CLD_MAJOR mismatch, please check version.cpp\n" );
-		}
-
-		if( lgRelease || nBetaVer > 0 )
-		{
-			// this expects a branch name starting with "c08.01"
-			sscanf( Branch.substr(4,2).c_str(), "%d", &nMinorLevel );
-			if( nMinorLevel != CLD_MINOR )
-				fprintf( ioQQQ, "PROBLEM - CLD_MINOR mismatch, please check version.cpp\n" );
-		}
-		
-		if( lgPatchTag )
-		{
-			// this expects a branch name like "c08.01_pl02"
-			sscanf( Branch.substr(9,2).c_str(), "%d", &nPatchLevel );
-			if( nPatchLevel != CLD_PATCH )
-				fprintf( ioQQQ, "PROBLEM - CLD_PATCH mismatch, please check version.cpp\n" );
-			// c08.00_pl00 is identical to release c08.00, so pass it off as the latter...
-			if( nPatchLevel == 0 )
-				lgReleaseTag = true;
-		}
-
-		string pps = ( isdigit(revision[0]) ) ? "r" : "";
-
-		ostringstream oss;
-		if( lgReleaseTag )
-			// this expects a branch name like "c08.01"
-			oss << Branch.substr(1,5);
-		else if( lgPatchTag )
-			// this expects a branch name like "c08.01_pl02"
-			oss << Branch.substr(1,5) << " (patch level " << nPatchLevel << ")";
-		else if( nBetaVer > 0 )
-			// this expects a branch name like "c08.00_rc1"
-			oss << Branch.substr(1,5) << " beta " << nBetaVer << " (prerelease)";
-		else if( lgReleaseBranch )
-			// this expects a branch name like "c08_branch"
-			oss << "(" << Branch << ", " << pps << revision << ", prerelease)";
-		else
-			// the branch name can be anything except "c??_branch"
-			oss << "(" << Branch << ", " << pps << revision << ", experimental)";
-		chVersion = oss.str();
-	}
 	else
 	{
-		// create a default version string in case HeadURL was not expanded
+		// create a default version string in case the code is not versioned with git
 
 		/* is this a release branch? */
 		lgReleaseBranch = false;
