b67d2d66ca1d548f9f8dd1fe0a8b786dd482cd3e
Gary Ferland <gary@g.uky.edu>
2021-11-19 13:24:57 -0500

    do local declaration of variables

--- source/ion_collis.cpp
+++ source/ion_collis.cpp
@@ -13,8 +13,6 @@ void ion_collis(
 	/* element number on c scale, H is 0 */
 	long int nelem )
 {
-	long int ion,
-		limit;
 	double DimaRate, crate;
 
 	DEBUG_ENTRY( "ion_collis()" );
@@ -26,7 +24,7 @@ void ion_collis(
 	 */
 
 	/* zero out rates below lowest ionization stage we will consider */
-	for( ion=0; ion < (dense.IonLow[nelem] - 1); ion++ )
+	for( long ion=0; ion < (dense.IonLow[nelem] - 1); ion++ )
 	{
 		ionbal.CollIonRate_Ground[nelem][ion][0] = 0.;
 		ionbal.CollIonRate_Ground[nelem][ion][1] = 0.;
@@ -35,10 +33,10 @@ void ion_collis(
 	/* chng logic changed to be precisely same as ion_solver */
 	/* >>chng 02 nov 08, change 2 to NISO */
 	/*limit = MIN2(nelem-2,dense.IonHigh[nelem]-1);*/
-	limit = MIN2(nelem-NISO,dense.IonHigh[nelem]-1);
+	long limit = MIN2(nelem-NISO,dense.IonHigh[nelem]-1);
 	ASSERT( limit < LIMELM );
 
-	for( ion=dense.IonLow[nelem]; ion <= limit; ion++ )
+	for( long ion=dense.IonLow[nelem]; ion <= limit; ion++ )
 	{
 		//Get the rate coefficients using either Dima or Hybrid
 		DimaRate = t_ADfA::Inst().coll_ion_wrapper( nelem, ion , phycon.te );
@@ -54,14 +52,14 @@ void ion_collis(
 			rfield.anu(Heavy.ipHeavy[nelem][ion]-1)* EN1RYD);
 	}
 
-	for( ion=dense.IonHigh[nelem]; ion <= nelem; ion++ )
+	for( long ion=dense.IonHigh[nelem]; ion <= nelem; ion++ )
 	{
 		ionbal.CollIonRate_Ground[nelem][ion][0] = 0.;
 		ionbal.CollIonRate_Ground[nelem][ion][1] = 0.;
 	}
 
 	/* check not rates are negative - in release mode this loop will optimize out */
-	for( ion=0; ion <= nelem; ion++ )
+	for( long ion=0; ion <= nelem; ion++ )
 	{
 		/* there can be no negative rates */
 		ASSERT( ionbal.CollIonRate_Ground[nelem][ion][0] >= 0. );
