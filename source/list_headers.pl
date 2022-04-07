76856f1a93622cf7cf2ebc2b2dfffafc5db51d24
Marios Chatzikos <mchatzikos@uky.edu>
2022-04-07 17:09:01 -0400

    Create file to hold unused header files
    list_headers.pl creates a file that shows all files that use each header
    file.  It takes some parsing to find if there are files not being used.
    
    The new file contains all unused header files.

--- source/list_headers.pl
+++ source/list_headers.pl
@@ -7,14 +7,19 @@ $hfiles='headers.txt';
 # List of header files  and C++ files using them  
 $result='listfiles.list'; 
 
+# List of unused header files
+$unused='headers_unused.txt';
+
 print "A list of header files will be placed in ", $hfiles , "\n";
-print "All source files using each header will be placed in ", $result , "\n" ;
+print "All source files using each header will be placed in ", $result , "\n";
+print "Unused header files will be placed in ", $unused , "\n";
 print "This will take a while...\n\n";
 
 #Start of main
 #system "ls *.h > $hfiles";
 open(ioHEADERS,">$hfiles"); #ioHEADERS-Output file
 open(LFILE,">$result");
+open(UFILE,">$unused");
 
 while(defined($header=glob("*.h")))
 {
@@ -27,7 +32,8 @@ while(<IHFILE>)
 {
   $header=$_;
   print LFILE "$header";
-  $header=~s/\n//;  
+  $header=~s/\n//;
+  $nuses = 0;
   while(defined($input=glob("*.h *.cpp")))  #Scanning through the C++ files
   {
      next if $header eq $input;
@@ -37,13 +43,15 @@ while(<IHFILE>)
        if($_=~/$header/)
        {
          print LFILE "\t$input";
+         $nuses++;
        }     
      }
   }
   print LFILE "\n\n"; 
+  print UFILE "$header\n"
+    if $nuses == 0;
 }
 close(IHFILE);
 close(LFILE);
+close(UFILE);
 #End of program
-
-  	
