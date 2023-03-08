a7a3ff92d17fea6e57b2bd866b94546588f8ec98
Gary J. Ferland <gary@g.uky.edu>
2023-03-08 09:20:38 -0500

    do not extrapolate UMIST rates to high temperatures

--- source/mole.h
+++ source/mole.h
@@ -572,6 +572,10 @@ bool parse_species_label( const char mylab[], ChemNuclideList &atomsLeftToRight,
 
 inline double hmrate4( double a, double b, double c, double te )
 {
+	/* UMIST rates are simple temperatuer power laws that
+	 * can become large at the high temperatures Cloudy
+	 * may encounter. Do not extrapolate to above T>1e3K */ 	
+	te = min(1000.,te); 
 	if( b == 0. && c == 0. )
 		return a;
 	else if( c == 0. )
