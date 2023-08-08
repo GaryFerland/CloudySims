3c9e0d4df44c5b81051a6eea4e7104f68505937d
Marios Chatzikos <mchatzikos@uky.edu>
2023-08-08 12:37:11 -0400

    Prepare for C23.00 release

--- source/prt_lines_helium.cpp
+++ source/prt_lines_helium.cpp
@@ -1,4 +1,4 @@
-/* This file is part of Cloudy and is copyright (C)1978-2019 by Gary J. Ferland and
+/* This file is part of Cloudy and is copyright (C)1978-2023 by Gary J. Ferland and
  * others.  For conditions of distribution and use see copyright notice in license.txt */
 /*lines_helium put He-like iso sequence into line intensity stack */
 /*TempInterp interpolates on a grid of values to produce predicted value at current Te.*/
