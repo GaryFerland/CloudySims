e879febf0f9cd9afba5a4f272784ce4a5516ee79
Peter van Hoof <p.vanhoof@oma.be>
2024-10-08 18:19:17 +0200

    Do not use the -fno-pie or -no-pie compiler flags.

--- source/capabilities.pl
+++ source/capabilities.pl
@@ -3,12 +3,6 @@
 $res = "";
 $os = `uname -s`;
 $arch = "";
-if( $os =~ /Darwin/ )
-{
-    my $tmp = `uname -m`;
-    chomp( $tmp );
-    $arch = "arm64" if( $tmp =~ m/arm64/ );
-}
 $version = `$ARGV[0] --version 2> /dev/null`;
 # remove comments in parentheses, they may or may not contain spaces
 $version =~ s/\(.*?\)//g;
@@ -63,27 +57,8 @@ close FOO;
 if( "$^O" ne "cygwin" ) {
     $ret = system( "$ARGV[0] -Wl,-export-dynamic -o tmp_cloudyconfig.out tmp_cloudyconfig.cpp > /dev/null 2>&1" );
     if( $ret == 0 ) {
-	$res .= "dynamic ";
+		$res .= "dynamic ";
     }
-}
-unlink "tmp_cloudyconfig.out";
-if( $arch ne "arm64" )
-{
-    # test if the -no-pie and -fno-pie flags are supported
-	# first try both flags
-	$npflags = "-no-pie -fno-pie";
-    $ret = system( "$ARGV[0] $npflags -o tmp_cloudyconfig.out tmp_cloudyconfig.cpp > /dev/null 2>&1" );
-    if( $ret == 0 ) {
-        $res .= "$npflags ";
-    }
-	else {
-		# try only the -fno-pie flag
-		$npflags = "-fno-pie";
-		$ret = system( "$ARGV[0] $npflags -o tmp_cloudyconfig.out tmp_cloudyconfig.cpp > /dev/null 2>&1" );
-		if( $ret == 0 ) {
-			$res .= "$npflags ";
-		}
-	}
 }
 unlink "tmp_cloudyconfig.cpp";
 unlink "tmp_cloudyconfig.out";
